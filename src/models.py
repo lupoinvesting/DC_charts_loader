e8e9872d7a425cbc4867e10d9cb449cbd03b2860
Lorenzo Cimarossa <l.cimarossa@gmail.com>
2025-06-10 16:59:13 +0200

    screenshot tool

--- src/models.py
+++ src/models.py
@@ -36,10 +36,13 @@ class ChartsData(ABC):
     def load_chart(self, index: Optional[int] = None):
         raise NotImplementedError("This method should be implemented by subclasses.")
 
+    def get_metadata(self, index: int) -> dict:
+        raise NotImplementedError("This method should be implemented by subclasses.")
+
 
 class ChartsDailyData(ChartsData):
     def __init__(self, dict_filename, data_filename):
-        self.charts = []
+        self.charts = pd.DataFrame()
         self.current_index = 0
         self.dict_filename = dict_filename
         self.data_filename = data_filename
@@ -52,16 +55,24 @@ class ChartsDailyData(ChartsData):
     def load_data(self):
         self.data = load_daily_df(self.data_filename)
 
+    def get_metadata(self, index: int) -> dict:
+        ticker = self.charts.ticker.iloc[index]
+        date = self.charts.date.iloc[index]
+        return {
+            "ticker": ticker,
+            "date_str": date.strftime("%Y-%m-%d"),
+            "date": date,
+            "timeframe": "1D",
+            "index": index,
+        }
+
     def load_chart(self, index: Optional[int] = None) -> tuple[pd.DataFrame, dict]:
         if index is None:
             index = self.current_index
 
-        ticker = self.charts.ticker.iloc[index]
-        date = self.charts.date.iloc[index]
+        metadata = self.get_metadata(index)
+
+        ticker = metadata["ticker"]
+        date = metadata["date"]
         df = load_daily_data(ticker, date, self.data)
-        metadata = {
-            "ticker": ticker,
-            "date": date,
-            "index": index,
-        }
         return df, metadata
--- src/ui.py
+++ src/ui.py
@@ -4,15 +4,11 @@ import pandas as pd
 
 
 def on_up(chart: Chart, chart_data: ChartsData):
-    print("Next chart")
-    print(f"Current index: {chart_data.current_index}")
     df, metadata = chart_data.next_chart()
     plot_chart(df, metadata, chart)
 
 
 def on_down(chart: Chart, chart_data: ChartsData):
-    print("Previous chart")
-    print(f"Current index: {chart_data.current_index}")
     df, metadata = chart_data.previous_chart()
     plot_chart(df, metadata, chart)
 
@@ -25,16 +21,46 @@ def plot_chart(df: pd.DataFrame, metadata: dict, chart: Chart) -> None:
     """
     chart.set(df)
     try:
-        chart.watermark(f"{metadata['ticker']} {metadata['date']}")
+        chart.watermark(
+            f"{metadata['ticker']} {metadata['timeframe']} {metadata['date_str']}"
+        )
     except AttributeError:
         chart.watermark(f"na")
 
 
-# def print_chart(df: pd.DataFrame):
-#     print("Chart data:")
-#     print(df.head())
-#     print(f"Ticker: {df.ticker.iloc[0]}")
-#     print(f"Date: {df.date.iloc[0]}")
-#     print(f"Shape: {df.shape}")
-#     print(f"Columns: {df.columns.tolist()}")
-#     print(f"Data types:\n{df.dtypes}")
+def save_screenshot(chart: Chart, chart_data: ChartsData, folder="screenshots") -> None:
+
+    img = chart.screenshot()
+    metadata = chart_data.get_metadata(chart_data.current_index)
+    filename = f"{folder}/{metadata['ticker']}_{metadata['date_str']}_screenshot.png"
+    with open(filename, "wb") as f:
+        f.write(img)
+    print(f"Screenshot saved to {filename}")
+
+
+def create_and_bind_chart(
+    chart_data: ChartsData,
+) -> Chart:
+    """
+    Creates a Chart object, plots data onto it, and binds hotkeys for user interactions.
+
+    Args:
+        df (pd.DataFrame): The data to be plotted on the chart.
+        metadata (dict): Metadata information used for plotting the chart.
+        chart_data (ChartsDailyData): Chart data object used by hotkey callback functions.
+
+    Returns:
+        Chart: The configured Chart object with hotkeys bound.
+
+    Hotkeys:
+        Shift+1: Triggers the `on_up` callback.
+        Shift+2: Triggers the `on_down` callback.
+        Shift+S: Triggers the `save_screenshot` callback.
+    """
+    chart = Chart()
+    df, metadata = chart_data.load_chart(0)
+    plot_chart(df, metadata, chart)
+    chart.hotkey("shift", 1, func=lambda _: on_up(chart, chart_data))
+    chart.hotkey("shift", 2, func=lambda _: on_down(chart, chart_data))
+    chart.hotkey("shift", "S", func=lambda _: save_screenshot(chart, chart_data))
+    return chart
